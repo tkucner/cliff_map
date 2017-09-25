bb6e7ceea6e5dac9030f6195cfecc5356b7eb6f0
Tomasz Kucner <tomasz.kucner@oru.se>
2017-09-26 00:03:06 +0200

    added colour coding for mixing factor and mean speed visualisation

--- src/cliff_marker_pub.cpp
+++ src/cliff_marker_pub.cpp
@@ -51,6 +51,35 @@ std::istream& operator>>(std::istream& str, CSVRow& data)
         return str;
 }
 
+
+
+
+bool getHeatMapColor(float value, float *red, float *green, float *blue)
+{
+  const int NUM_COLORS = 4;
+  static float color[NUM_COLORS][3] = { {0,0,1}, {0,1,0}, {1,1,0}, {1,0,0} };
+    // A static array of 4 colors:  (blue,   green,  yellow,  red) using {r,g,b} for each.
+
+  int idx1;        // |-- Our desired color will be between these two indexes in "color".
+  int idx2;        // |
+  float fractBetween = 0;  // Fraction between "idx1" and "idx2" where our value is.
+
+  if(value <= 0)      {  idx1 = idx2 = 0;            }    // accounts for an input <=0
+  else if(value >= 1)  {  idx1 = idx2 = NUM_COLORS-1; }    // accounts for an input >=0
+  else
+  {
+    value = value * (NUM_COLORS-1);        // Will multiply value by 3.
+    idx1  = floor(value);                  // Our desired color will be after this index.
+    idx2  = idx1+1;                        // ... and before this index (inclusive).
+    fractBetween = value - float(idx1);    // Distance between the two indexes (0-1).
+  }
+
+  *red   = (color[idx2][0] - color[idx1][0])*fractBetween + color[idx1][0];
+  *green = (color[idx2][1] - color[idx1][1])*fractBetween + color[idx1][1];
+  *blue  = (color[idx2][2] - color[idx1][2])*fractBetween + color[idx1][2];
+}
+
+
 int main(int argc, char **argv){
         ros::init(argc, argv, "cliff_marker_pub");
         ros::NodeHandle n;
@@ -101,13 +130,16 @@ int main(int argc, char **argv){
                         yaws_array_msg.markers[array_index].pose.position.z = 0.5;
                         yaws_array_msg.markers[array_index].pose.orientation =
                                 tf::createQuaternionMsgFromRollPitchYaw(0, 0, atof(rows[array_index][2].c_str()));
-                        yaws_array_msg.markers[array_index].scale.x = 0.5;
+                        yaws_array_msg.markers[array_index].scale.x = atof(rows[array_index][3].c_str());
                         yaws_array_msg.markers[array_index].scale.y = 0.1;
                         yaws_array_msg.markers[array_index].scale.z = 0.1;
+                        float r,g,b;
+                        getHeatMapColor(atof(rows[array_index][4].c_str()),&r,&g,&b);
+
                         yaws_array_msg.markers[array_index].color.a = 1.0;
-                        yaws_array_msg.markers[array_index].color.r = 0.0;
-                        yaws_array_msg.markers[array_index].color.b = 0.0;
-                        yaws_array_msg.markers[array_index].color.g = 1.0;
+                        yaws_array_msg.markers[array_index].color.r = r;
+                        yaws_array_msg.markers[array_index].color.b = g;
+                        yaws_array_msg.markers[array_index].color.g = b;
                         array_index++;
                 }
                 cliff_pub.publish(yaws_array_msg);
